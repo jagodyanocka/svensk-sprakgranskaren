3d0bd97b35076629e663c126fee59c702b0719e2
Jagoda <jagoda.kosewska@gmail.com>
2026-08-24 15:41:19 +0200

    change layouts and colors

--- app/gradio_app/gradio_app.py
+++ app/gradio_app/gradio_app.py
@@ -2,30 +2,37 @@ import gradio as gr
 
 from app.ai.ai_clients.ollama_client import react_to_user_poor_swedish
 
-theme = gr.themes.Soft.from_hub("hmb/amethyst")
+theme = gr.themes.Soft.from_hub("hmb/amethyst").set(
+    input_background_fill_focus_dark="#13131F",
+    input_background_fill_focus="#F4F2FF",
+)
 
-with gr.Blocks(theme=theme) as tutor:
+with gr.Blocks(
+        theme=theme
+
+) as tutor:
+    gr.HTML("<h2 style='text-align:center; font-size:2.2rem;'>Språkgranskaren</h2>")
+
     with gr.Row():
-        gr.Markdown("# Språkgranskaren")
 
-    with gr.Row():
+        with gr.Column():
+            user_input = gr.Textbox(
+                label="Enter your broken Swedish here:",
+                placeholder="Skriv din bristfälliga svenska.",
+                lines=10,
+            )
+            with gr.Row(elem_classes="button-row"):
+                correct_btn = gr.Button("Correct me!", variant="primary")
+        with gr.Column():
+            out = gr.Textbox(
+                lines=10,
+                label="Korrigerat svenskt",
+                placeholder="...AI output will appear here",
+                interactive=False
+            )
+            with gr.Row(elem_classes="button-row"):
+                clear_btn = gr.Button("Clear", variant="primary")
 
-        user_input = gr.Textbox(
-            label="Enter your broken Swedish here:",
-            placeholder="Skriv din bristfälliga svenska.",
-            lines=10,
-            scale=5
-        )
-        with gr.Row(elem_classes="button-row"):
-            correct_btn = gr.Button("Correct me!", variant="primary")
-
-        out = gr.Textbox(
-            lines=10,
-            label="Korrigerat svenskt",
-            placeholder="...AI output will appear here",
-            scale=5,
-            interactive=False
-        )
     correct_btn.click(
         react_to_user_poor_swedish,
         inputs=user_input,
@@ -39,6 +46,6 @@ with gr.Blocks(theme=theme) as tutor:
     )
 
 
-tutor.launch(share=True)
+tutor.launch(run_history=False)
 
 
